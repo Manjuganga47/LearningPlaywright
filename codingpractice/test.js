57684ef22b2c5f75e47e49e45f20f0a55101a111
Maheshwari Gangadhari <maheshwarig92@gmail.com>
2026-08-15 21:13:26 +0530

    commiting 10 chpater

---
+++ codingpractice/test.js
@@ -0,0 +1,28 @@
+console.log(x);
+ var x = 5
+ console.log(2 ** 3)
+ console.log('5' + 3)
+ console.log(true + true)
+ console.log(0 || 'hello')
+ console.log(0 ?? 'hello')
+ console.log(10 % 3)
+ let a = 5;
+let b = a++;
+console.log(a, b);
+if ('')
+     { console.log('yes');
+
+      }
+       else { console.log('no'); }
+
+console.log(Boolean([]))  
+
+let status = 200;
+if (status === 200) {
+  console.log("OK");
+} else if (status === 404) {
+  console.log("Not Found");
+} else {
+  console.log("Other");
+}
+for (let i = 0; i < 5; i++) { if (i === 3) break; } console.log(i);
